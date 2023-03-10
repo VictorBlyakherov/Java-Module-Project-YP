7d99872007dea821e9f526cba03c450a6bdea9f9
Viktor Blyakherov <xxxxxvic@yandex.ru>
2023-03-10 16:24:21 +0300

    First commit

---
+++ src/main/java/Item.java
@@ -0,0 +1,39 @@
+public class Item {
+    final private String name;
+    final private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (Double.compare(item.price, price) != 0) return false;
+        return name != null ? name.equals(item.name) : item.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+}
